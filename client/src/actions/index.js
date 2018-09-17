a26ec197d6f9df8901b627a44d5a08f1eac8a3d5
Ele_hopper <elenacerezo83@gmail.com>
2018-09-18 00:26:35 +0200

    dockerized app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 export const SHOW_MOBILES = "SHOW_MOBILES";
-export const SHOW_DETAILS = "SHOW_DETAILS"
+export const SHOW_DETAILS = "SHOW_DETAILS";
 
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000'
+})
 export function showMobiles() {
     return (dispatch, getState) => {
-        axios.get("/api/catalog").then(response => {
+        axiosInstance.get("/api/catalog").then(response => {
             dispatch({ type: SHOW_MOBILES, payload: response.data });
         });
     }
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
 app.get("/api/catalog", (req, res) => {
     const catalog = [
         {
@@ -24,6 +31,8 @@ app.get("/api/catalog", (req, res) => {
             visible: false
         }
     ];
+
+
 
     res.json(catalog);
 })
